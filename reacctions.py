947aeea4ef362caf3323fdb77927eb91ac25e72f
ZikQ1337 <63538873+ZikQ1337@users.noreply.github.com>
2020-06-25 17:40:03 +0300

    Update reacctions.py

--- reacctions.py
+++ reacctions.py
@@ -1,6 +1,5 @@
 @Bot.event
 async def on_raw_reaction_add(payload):
     if payload.message_id == ВашАйди and payload.emoji.name == ВашЭмоджи:
-        for guild in Bot.guilds:
-            role = discord.utils.get(guild.roles, id = ВашАйди)
-            await payload.member.add_roles(role)
+        role = discord.utils.get(payload.guild.roles, id = ВашАйди)
+        await payload.member.add_roles(role)
