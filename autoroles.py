fae4611864efb45a3960803bb562b8111b2cecfd
ZikQ1337 <63538873+ZikQ1337@users.noreply.github.com>
2021-03-03 18:52:59 +0300

    Update autoroles.py

--- autoroles.py
+++ autoroles.py
@@ -1,7 +1,7 @@
 @Bot.event
 async def on_member_join(member):
     channel = Bot.get_channel(ВашАйди)
-    role = discord.utils.get(member.guild.roles,id=ВашАйди)
+    role = member.guild.get_role(ВашАйди)
     await member.add_roles(role)
     await channel.send(f"{member.mention} Добро пожаловать на сервер!")
 @Bot.event
